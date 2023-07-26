c27d9f5f2c4b1b3802c0a3cd404ffe07723ee8c4
Uzytkownik <kzielecki@op.pl~>
2023-07-26 20:21:54 +0200

    [change] - RestControllerAdvice created

---
+++ src/main/java/com/sda/rentalloapp/controller/CarRestControllerAdvisor.java
@@ -0,0 +1,35 @@
+package com.sda.rentalloapp.controller;
+import com.sda.rentalloapp.Exception.WrongCarIdException;
+import com.sda.rentalloapp.dto.ResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+@Slf4j
+public class CarRestControllerAdvisor {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(WrongCarIdException.class)
+    public ResponseDto handleCarNotFoundException(WrongCarIdException exc) {
+        String path = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .build().
+                getPath();
+
+       // log.warn();
+        return ResponseDto.
+                builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND.value())
+                .error("Not Found")
+                .message(exc.getMessage())
+                // TODO: get path
+                .path(path)
+                .build();
+    }
+}
