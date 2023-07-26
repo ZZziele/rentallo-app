88edc0fdb0f1942aca2cf50b4882eec375eb03f2
Uzytkownik <kzielecki@op.pl~>
2023-07-26 18:07:19 +0200

    [change] -  CarMapper from Entity to dto implemented

--- src/main/java/com/sda/rentalloapp/mapper/CarMapper.java
+++ src/main/java/com/sda/rentalloapp/mapper/CarMapper.java
@@ -2,6 +2,7 @@ package com.sda.rentalloapp.mapper;
 
 import com.sda.rentalloapp.domain.Car;
 import com.sda.rentalloapp.dto.CarDto;
+import com.sda.rentalloapp.dto.PicturesDto;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +10,23 @@ import org.springframework.stereotype.Component;
 public class CarMapper implements Mapper<Car, CarDto> {
     @Override
     public CarDto fromEntityToDto(Car entity) {
-        return null;
+        return CarDto.builder()
+                .id(entity.getId())
+                .model(entity.getModel())
+                .brand(entity.getBrand())
+                .fuelType(entity.getFuelType())
+                .engineType(entity.getEngineType())
+                .bodyType(entity.getBodyType())
+                .numberOfSeats(entity.getNumberOfSeats())
+                .trunkCapacityInLitres(entity.getTrunkCapacityOnLitres())
+                .combustionPer100Km(entity.getCombustionPer100Km())
+                .bodySerialNumber(entity.getBodySerialNumber())
+                .pricePerDayInPolishGrosz(entity.getPricePedDayInPolishGrosz())
+                .available(entity.isAvailable())
+                .rangeInKm(entity.getRangeInKm())
+                .pictures( new PicturesDto(entity.getPictures().getMainPictureUrl(),entity.getPictures().getPicturesUrl()))
+                .build();
+
     }
 
     @Override
