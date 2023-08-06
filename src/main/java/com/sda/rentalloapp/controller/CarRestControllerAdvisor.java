53dc252b412e0fad51ecbf682eabcc25a87122d3
Uzytkownik <kzielecki@op.pl~>
2023-08-06 12:13:08 +0200

    [add]  - endpoint for new cars - in progress

--- src/main/java/com/sda/rentalloapp/controller/CarRestControllerAdvisor.java
+++ src/main/java/com/sda/rentalloapp/controller/CarRestControllerAdvisor.java
@@ -1,9 +1,12 @@
 package com.sda.rentalloapp.controller;
 import com.sda.rentalloapp.Exception.WrongCarIdException;
+import com.sda.rentalloapp.dto.CarDto;
 import com.sda.rentalloapp.dto.ResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -16,10 +19,7 @@ public class CarRestControllerAdvisor {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(WrongCarIdException.class)
     public ResponseDto handleCarNotFoundException(WrongCarIdException exc) {
-        String path = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .build().
-                getPath();
+        String path = getCurrentRequestPath();
 
        // log.warn();
         return ResponseDto.
@@ -30,5 +30,28 @@ public class CarRestControllerAdvisor {
                 .message(exc.getMessage())
                 .path(path)
                 .build();
+    }
+
+    private static String getCurrentRequestPath() {
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .build().
+                getPath();
+    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseDto handleBadRequest(MethodArgumentNotValidException exc){
+    String path = getCurrentRequestPath();
+
+    return ResponseDto.builder()
+            .timestamp(LocalDateTime.now())
+            .status(HttpStatus.BAD_REQUEST.value())
+            .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+            .path(path)
+            .message(exc.getMessage())
+            .build();
+
     }
 }
