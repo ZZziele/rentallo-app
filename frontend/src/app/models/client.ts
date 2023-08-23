a4df2719a1fad8dbc5861892b5d2e01b1648cf35
Uzytkownik <kzielecki@op.pl~>
2023-08-23 18:27:43 +0200

    [change] - data model for client created

---
+++ frontend/src/app/models/client.ts
@@ -0,0 +1,13 @@
+import {Address} from "./address";
+export interface Client {
+  id: number;
+  name: string;
+  surname: string;
+  pesel: string;
+  email: string;
+  dateOfBirth: string;
+  phone: string;
+  // TODO: change name of property
+  addressDto: Address;
+  accountCreated: string
+}
