56ca21ea69c0014c5815655d930f7fec42aff2a0
Uzytkownik <kzielecki@op.pl~>
2023-08-28 20:14:44 +0200

    [change] - ui config controller - engine types implemented

--- backend-service/src/main/java/com/sda/rentalloapp/controller/UiConfigController.java
+++ backend-service/src/main/java/com/sda/rentalloapp/controller/UiConfigController.java
@@ -1,6 +1,7 @@
 package com.sda.rentalloapp.controller;
 
 import com.sda.rentalloapp.domain.enumeration.BodyType;
+import com.sda.rentalloapp.domain.enumeration.EngineType;
 import com.sda.rentalloapp.domain.enumeration.FuelType;
 import com.sda.rentalloapp.dto.ConfigItem;
 import lombok.extern.slf4j.Slf4j;
@@ -29,6 +30,15 @@ public class UiConfigController {
 
         return Arrays.stream( BodyType.values())
                 .map(bodyType -> new ConfigItem<>(bodyType.name(),bodyType.description()))
+                .toList();
+    }
+
+
+    @GetMapping("/engine-types")
+    public List<ConfigItem<String,String>> getEngineTypes(){
+
+        return Arrays.stream( EngineType.values())
+                .map(engineTypes -> new ConfigItem<>(engineTypes.name(),engineTypes.description()))
                 .toList();
     }
 }
