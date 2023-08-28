7d4722f7a64e9a5ddd54e06f186b692bc1a80d1f
Uzytkownik <kzielecki@op.pl~>
2023-08-28 20:01:07 +0200

    [change] - ui config controller - fuel types implemented

--- backend-service/src/main/java/com/sda/rentalloapp/controller/UiConfigController.java
+++ backend-service/src/main/java/com/sda/rentalloapp/controller/UiConfigController.java
@@ -1,12 +1,25 @@
 package com.sda.rentalloapp.controller;
 
+import com.sda.rentalloapp.domain.enumeration.FuelType;
+import com.sda.rentalloapp.dto.ConfigItem;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("/api/")
 public class UiConfigController {
+    @GetMapping("/fuel-types")
+    public List<ConfigItem<String,String>> getFuelTypes(){
 
+        return Arrays.stream( FuelType.values())
+                .map(fuelType -> new ConfigItem<>(fuelType.name(),fuelType.fuelDescription()))
+                .toList();
+
+    }
 }
--- backend-service/src/main/java/com/sda/rentalloapp/domain/enumeration/FuelType.java
+++ backend-service/src/main/java/com/sda/rentalloapp/domain/enumeration/FuelType.java
@@ -1,21 +1,15 @@
 package com.sda.rentalloapp.domain.enumeration;
-
 public enum FuelType {
-    GAS("gas fuel") ,
-    OIL("oil fuel"),
-    GASOLINE("gasoline fuel"),
-    ELECTRIC("electricity"),
-    HYBRID ("hybrid");
-
-
-
+    GAS("Gas fuel"),
+    OIL("Oil fuel"),
+    GASOLINE("Gasoline fuel"),
+    ELECTRIC("Electricity"),
+    HYBRID("Hybrid");
     FuelType(String fuelDesc) {
-        this.fueltDesc = fuelDesc;
+        this.fuelDesc = fuelDesc;
     }
-
-    String fueltDesc;
-
-    public String getFueltDesc() {
-        return fueltDesc;
+    public String fuelDescription() {
+        return fuelDesc;
     }
+    private String fuelDesc;
 }
